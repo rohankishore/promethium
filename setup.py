3aff3e3784173a39ee3cb5235845c2979e6dbbbc
Rohan Kishore <rohankishore746@outlook.com>
2025-10-17 23:44:29 +0530

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='promethium',
-    version='0.1',
+    version='0.1.0',
+    author='Rohan Kishore',
+    author_email='rohan@example.com',
+    description='A Pythonic interface to a rich, offline database of chemical information.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rohankishore/promethium',
     packages=find_packages(),
+    package_data={
+        'promethium': ['data/*.csv'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
     install_requires=[
+        'pandas',
+        'numpy',
     ],
 )
