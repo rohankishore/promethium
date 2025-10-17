1db6c0126ac6efcbded3e74242656681b965b948
Rohan Kishore <rohankishore746@outlook.com>
2025-10-17 23:58:14 +0530

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='promethium',
+    name='promethium-py',
     version='0.1.0',
     author='Rohan Kishore',
     author_email='rohan@example.com',
