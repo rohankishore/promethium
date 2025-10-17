c62ca30ef21cbe73893785bb0b67b44af99432d0
Rohan Kishore <rohankishore746@outlook.com>
2025-10-17 23:53:05 +0530

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    license='MIT',
     python_requires='>=3.8',
     install_requires=[
         'pandas',
